dc3b46ec018131d4dbfbba1ea990b84acb3b6f00
mabsattar <mabsattar@proton.me>
2026-09-24 02:45:26 +0530

    import contract and devtools, add run and mintNFTOnContract functions

--- foundry-nft-cu/script/Interactions.s.sol
+++ foundry-nft-cu/script/Interactions.s.sol
@@ -3,7 +3,22 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {BasicNFT} from "../src/BasicNFT.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
 contract MintBasicNFT is Script {
-    function run() external {}
+    string public constant TOKENURI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("BasicNFT", block.chainid);
+
+        mintNFTOnContract(mostRecentlyDeployed);
+    }
+
+    function mintNFTOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        BasicNFT(contractAddress).mintNFT(TOKENURI);
+        vm.stopBroadcast();
+    }
 }
